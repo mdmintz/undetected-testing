36a4bf2d9ba534d24193df09c6be282feb02a3a6
Michael Mintz <mdmintz@gmail.com>
2023-02-03 22:25:34 -0500

    Update tests

--- test_verify_undetected.py
+++ test_verify_undetected.py
@@ -8,14 +8,28 @@ if __name__ == "__main__":
 
 
 class UndetectedTest(BaseCase):
+    def verify_success(self):
+        self.assert_text("OH YEAH, you passed!", "h1", timeout=5.95)
+        self.post_message("Selenium wasn't detected!", duration=2.8)
+        self._print("\n Success! Website did not detect Selenium! ")
+
+    def fail_me(self):
+        self.fail('Selenium was detected! Try using: "pytest --uc"')
+
     def test_browser_is_undetected(self):
-        if not self.undetectable or not self.incognito:
-            self.get_new_driver(undetectable=True, incognito=True)
         self.open("https://nowsecure.nl/#relax")
+        self.hide_elements("iframe")
         try:
-            self.assert_text("OH YEAH, you passed!", "h1", timeout=7.75)
-            self.post_message("Selenium wasn't detected!", duration=2.8)
-            self._print("\n Success! Website did not detect Selenium! ")
-            self.save_screenshot_to_logs()
+            self.verify_success()
         except Exception:
-            self.fail('Selenium was detected! Try using: "pytest --uc"')
+            if self.is_element_visible('input[value*="Verify"]'):
+                self.click('input[value*="Verify"]')
+            elif self.is_element_visible('iframe[title*="challenge"]'):
+                self.switch_to_frame('iframe[title*="challenge"]')
+                self.click("span.mark")
+            else:
+                self.fail_me()
+            try:
+                self.verify_success()
+            except Exception:
+                self.fail_me()
--- verify_undetected.py
+++ verify_undetected.py
@@ -3,13 +3,43 @@ from seleniumbase import page_actions
 from seleniumbase import DriverContext
 
 
-with DriverContext(uc=True, incognito=True) as driver:
+def verify_success(driver):
     driver.get("https://nowsecure.nl/#relax")
     page_actions.wait_for_text(
         driver, "OH YEAH, you passed!", "h1", by="css selector"
     )
     print("\n Success! Website did not detect Selenium!")
+
+
+def fail_me():
+    raise Exception('Selenium was detected! Try using: "pytest --uc"')
+
+
+with DriverContext(uc=True, incognito=True) as driver:
+    driver.get("https://nowsecure.nl/#relax")
+    try:
+        verify_success(driver)
+    except Exception:
+        if page_actions.is_element_visible('input[value*="Verify"]'):
+            element = driver.find_element(
+                "css selector", 'input[value*="Verify"]'
+            )
+            element.click()
+        elif page_actions.is_element_visible(
+            "css selector", 'iframe[title*="challenge"]'
+        ):
+            element = driver.find_element(
+                "css selector", 'iframe[title*="challenge"]'
+            )
+            driver.switch_to.frame(element)
+            driver.find_element("css selector", "span.mark").click()
+        else:
+            fail_me()
+        try:
+            verify_success(driver)
+        except Exception:
+            fail_me()
     time.sleep(2)
     screenshot_name = "now_secure_image.png"
     driver.save_screenshot(screenshot_name)
-    print("\nScreenshot saved to: %s" % screenshot_name)
+    print("\nScreenshot saved to: %s\n" % screenshot_name)
