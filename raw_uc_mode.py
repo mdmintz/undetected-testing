fcee7ce44ad0605f4a5593600f0894f437379a5e
Michael Mintz <mdmintz@gmail.com>
2024-06-20 01:05:15 -0400

    Update GitHub Actions

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -1,19 +1,13 @@
-"""SB Manager using UC Mode for evading bot-detection."""
+import pyautogui
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
-    sb.driver.uc_open_with_tab("https://nowsecure.nl/#relax")
-    sb.sleep(1.2)
-    if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
-        sb.get_new_driver(undetectable=True)
-        sb.driver.uc_open_with_reconnect(
-            "https://nowsecure.nl/#relax", reconnect_time=3
-        )
-        sb.sleep(1.2)
-    if not sb.is_text_visible("OH YEAH, you passed!", "h1"):
-        if sb.is_element_visible('iframe[src*="challenge"]'):
-            with sb.frame_switch('iframe[src*="challenge"]'):
-                sb.click("span.mark")
-                sb.sleep(2)
-    sb.activate_demo_mode()
-    sb.assert_text("OH YEAH, you passed!", "h1", timeout=3)
+with SB(uc=True, test=True, xvfb=True) as sb:
+    url = "https://seleniumbase.io/hobbit/login"
+    sb.driver.uc_open_with_disconnect(url)
+    sb.sleep(3)
+    pyautogui.press("\t")
+    pyautogui.press(" ")
+    sb.sleep(3)
+    sb.connect()
+    sb.assert_text("Welcome to Middle Earth!", "h1")
+    print(sb.get_text("h1"))
