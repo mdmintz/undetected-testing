fd8274b0567fcc57f5a489c0d0a1a7c6905b1a96
Michael Mintz <mdmintz@gmail.com>
2024-06-20 13:59:54 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -22,3 +22,39 @@ with SB(uc=True, test=True) as sb:
     sb.connect()
     sb.assert_text("Welcome to Middle Earth!", "h1")
     print(sb.get_text("h1"))
+
+
+with SB(uc=True, test=True) as sb:
+    import pyautogui
+    import Xlib.display
+    from sbvirtualdisplay.display import Display
+    disp = Display(
+        visible=True, size=(1366, 768), backend="xvfb", use_xauth=True
+    )
+    disp.start()
+    pyautogui._pyautogui_x11._display = (
+        Xlib.display.Display(os.environ['DISPLAY'])
+    )
+    url = "https://seleniumbase.io/apps/brotector"
+    sb.uc_open_with_reconnect(url, 3)
+    print(sb.get_text("html"))
+
+with SB(uc=True, test=True) as sb:
+    import pyautogui
+    import Xlib.display
+    from sbvirtualdisplay.display import Display
+    disp = Display(
+        visible=True, size=(1366, 768), backend="xvfb", use_xauth=True
+    )
+    disp.start()
+    pyautogui._pyautogui_x11._display = (
+        Xlib.display.Display(os.environ['DISPLAY'])
+    )
+    url = "https://seleniumbase.io/apps/brotector"
+    sb.driver.uc_open_with_disconnect(url)
+    sb.sleep(3)
+    pyautogui.press("\t")
+    pyautogui.press(" ")
+    sb.sleep(3)
+    sb.connect()
+    print(sb.get_text("html"))
