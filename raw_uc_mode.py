ce523c6174e5674472c6e8c7844f35ce42a55285
Michael Mintz <mdmintz@gmail.com>
2024-06-20 17:31:05 -0400

    Update the example

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -12,39 +12,23 @@ pyautogui._pyautogui_x11._display = (
     Xlib.display.Display(os.environ['DISPLAY'])
 )
 
-with SB(uc=True, test=True, headed=True) as sb:
+with SB(uc=True, test=True, rtf=True, headed=True) as sb:
     url = "https://seleniumbase.io/hobbit/login"
     sb.driver.uc_open_with_disconnect(url)
-    sb.sleep(3)
+    sb.sleep(2.5)
     pyautogui.press("\t")
     pyautogui.press(" ")
-    sb.sleep(3)
+    sb.sleep(2.5)
     sb.connect()
     sb.assert_text("Welcome to Middle Earth!", "h1")
     print(sb.get_text("h1"))
 
-
-with SB(uc=True, test=True, headed=True) as sb:
-    url = "https://seleniumbase.io/apps/brotector"
-    sb.uc_open_with_reconnect(url, 3)
-    print(sb.get_text("html"))
-
-with SB(uc=True, test=True, headed=True) as sb:
+with SB(uc=True, test=True, rtf=True, headed=True) as sb:
     url = "https://seleniumbase.io/apps/brotector"
     sb.driver.uc_open_with_disconnect(url)
-    sb.sleep(3)
+    sb.sleep(2.5)
     pyautogui.press("\t")
     pyautogui.press(" ")
-    sb.sleep(3)
+    sb.sleep(2.5)
     sb.connect()
-    print(sb.get_text("html"))
-
-with SB(uc=True, test=True, headed=True) as sb:
-    url = "https://user-agent-client-hints.glitch.me/"
-    sb.uc_open_with_reconnect(url, 3)
-    print(sb.get_text("html"))
-
-with SB(test=True, headed=True) as sb:
-    url = "https://user-agent-client-hints.glitch.me/"
-    sb.open(url)
-    print(sb.get_text("html"))
+    sb.assert_text("SUCCESS", "#pText")
