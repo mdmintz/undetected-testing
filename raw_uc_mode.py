6adcb1ca815f5a658388db525d74a4decc13af5b
Michael Mintz <mdmintz@gmail.com>
2024-06-21 11:25:24 -0400

    Debugging

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -3,20 +3,19 @@ from seleniumbase import SB
 import Xlib.display
 from sbvirtualdisplay.display import Display
 
+print(os.environ['DISPLAY'])
 disp = Display(
     visible=True, size=(1366, 768), backend="xvfb", use_xauth=True
 )
 disp.start()
+print(os.environ['DISPLAY'])
 import pyautogui  # noqa
 print(pyautogui._pyautogui_x11._display)
 pyautogui._pyautogui_x11._display = (
     Xlib.display.Display(os.environ['DISPLAY'])
 )
 print(pyautogui._pyautogui_x11._display)
-pyautogui._pyautogui_x11._display = (
-    Xlib.display.Display(os.environ['DISPLAY'])
-)
-print(pyautogui._pyautogui_x11._display)
+print(os.environ['DISPLAY'])
 
 with SB(uc=True, test=True, rtf=True, headed=True) as sb:
     url = "https://seleniumbase.io/hobbit/login"
