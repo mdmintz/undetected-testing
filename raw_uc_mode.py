1265278d8fdb8303cc0ce856a5ca412c8788319d
Michael Mintz <mdmintz@gmail.com>
2024-06-22 16:11:00 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -14,7 +14,9 @@ with SB(uc=True, test=True, rtf=True) as sb:
 with SB(uc=True, test=True) as sb:
     url = "https://www.virtualmanager.com/en/login"
     sb.uc_open_with_reconnect(url, 4)
+    print(sb.get_current_url())
     sb.uc_gui_handle_cf()  # Ready if needed!
+    print(sb.get_current_url())
     sb.assert_element('input[name*="email"]')
     sb.assert_element('input[name*="login"]')
     sb.set_messenger_theme(location="bottom_center")
