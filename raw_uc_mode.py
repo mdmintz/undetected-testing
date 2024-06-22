d97f1979248265dd58b7d0f64f1dc64b6cde7791
Michael Mintz <mdmintz@gmail.com>
2024-06-22 01:51:26 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -2,15 +2,12 @@
 import os
 from seleniumbase import SB
 
-print("DISPLAY" in os.environ.keys())
-if "DISPLAY" in os.environ.keys():
-    print(os.environ['DISPLAY'])
-
 with SB(uc=True, test=True, rtf=True, xvfb=True) as sb:
     url = "https://seleniumbase.io/hobbit/login"
     sb.uc_open_with_disconnect(url, 2)
     sb.uc_gui_press_keys("\t ")
     sb.reconnect(2)
+    print(sb.get_current_url())
     sb.assert_text("Welcome to Middle Earth!", "h1")
     sb.set_messenger_theme(location="bottom_center")
     sb.post_message("SeleniumBase wasn't detected!")
