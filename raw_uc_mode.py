ec7aea4af879a9d531d14373b8697902708ff31b
Michael Mintz <mdmintz@gmail.com>
2024-06-22 01:03:30 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -1,10 +1,10 @@
 """UC Mode has PyAutoGUI methods for CAPTCHA-bypass."""
+import os
 from seleniumbase import SB
 
 print("DISPLAY" in os.environ.keys())
 if "DISPLAY" in os.environ.keys():
     print(os.environ['DISPLAY'])
-print(pyautogui._pyautogui_x11._display)
 
 with SB(uc=True, test=True, rtf=True, xvfb=True) as sb:
     url = "https://seleniumbase.io/hobbit/login"
