eacdf17ec0a463b51247b4deea6842f9382c508b
Michael Mintz <mdmintz@gmail.com>
2024-09-09 20:39:12 -0400

    Update tests

--- no_driver.py
+++ no_driver.py
@@ -1,10 +1,12 @@
 import nodriver
-from sbvirtualdisplay.display import Display
+import time
+from pyvirtualdisplay.display import Display
 
 
 async def main():
     browser = await nodriver.start()
     page = await browser.get("https://www.virtualmanager.com/en/login")
+    time.sleep(3)
     print(await page.evaluate("document.title"))
 
 if __name__ == "__main__":
