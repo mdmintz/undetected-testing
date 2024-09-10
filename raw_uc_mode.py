0949abfb550bb32e0e705bb284cd1262717bedea
Michael Mintz <mdmintz@gmail.com>
2024-09-10 16:35:08 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -2,6 +2,16 @@
 from seleniumbase import SB
 from seleniumbase import config as sb_config
 
+with SB(uc=True, test=True, rtf=True) as sb:
+    url = "https://seleniumbase.io/hobbit/login"
+    sb.uc_open_with_disconnect(url, 3)
+    sb.uc_gui_press_keys("\t ")
+    sb.reconnect(3)
+    print(sb.get_current_url())
+    sb.assert_text("Welcome to Middle Earth!", "h1")
+    sb.set_messenger_theme(location="bottom_center")
+    sb.post_message("SeleniumBase wasn't detected!")
+
 with SB(uc=True, test=True) as sb:
     import pyautogui
     url = "https://www.virtualmanager.com/en/login"
