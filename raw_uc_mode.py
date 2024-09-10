401264b4a1146d5cb6d6dea84a1b6897a7ed7904
Michael Mintz <mdmintz@gmail.com>
2024-09-10 10:37:53 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -13,9 +13,15 @@ with SB(uc=True, test=True, rtf=True) as sb:
 
 with SB(uc=True, test=True) as sb:
     url = "https://www.virtualmanager.com/en/login"
-    sb.uc_open_with_reconnect(url, 4)
-    print(sb.get_page_title())
-    sb.uc_gui_click_captcha()  # Only if needed
+    sb.uc_open_with_reconnect(url, 6)
+    sb.uc_gui_click_x_y(228, 387)
+    sb.sleep(3)
+    sb.uc_gui_click_x_y(228, 387)
+    sb.sleep(3)
+    sb.uc_gui_click_x_y(228, 387)
+    sb.sleep(3)
+    # print(sb.get_page_title())
+    # sb.uc_gui_click_captcha()  # Only if needed
     print(sb.get_page_title())
     sb.assert_element('input[name*="email"]')
     sb.assert_element('input[name*="login"]')
