b6ffd05d20b7f0a5f9de5c752dce74e86a0006ff
Michael Mintz <mdmintz@gmail.com>
2024-09-10 16:03:48 -0400

    Update a script

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -1,17 +1,26 @@
 """UC Mode has PyAutoGUI methods for CAPTCHA-bypass."""
 from seleniumbase import SB
+from seleniumbase import config as sb_config
 
 with SB(uc=True, test=True) as sb:
     import pyautogui
     url = "https://www.virtualmanager.com/en/login"
     sb.uc_open_with_reconnect(url, 6)
     print(sb.get_page_title())
-    sb.disconnect()
-    pyautogui.moveTo(228, 387, 1.05, pyautogui.easeOutQuad)
-    sb.sleep(0.056)
-    pyautogui.click()
-    sb.sleep(3)
-    sb.reconnect()
+    sb.uc_gui_click_captcha()
+    print(sb.get_page_title())
+    if (
+        "Just a moment" in sb.get_page_title()
+        and hasattr(sb_config, "_saved_cf_x_y")
+    ):
+        sb.uc_open_with_disconnect()
+        sb.sleep(4)
+        x, y = sb_config._saved_cf_x_y
+        pyautogui.moveTo(x, y, 1.05, pyautogui.easeOutQuad)
+        sb.sleep(0.056)
+        pyautogui.click()
+        sb.sleep(3)
+        sb.reconnect()
     print(sb.get_page_title())
     sb.assert_element('input[name*="email"]')
     sb.assert_element('input[name*="login"]')
