6b48a9538ac445d2a85227e213d5d370b5976bbf
Michael Mintz <mdmintz@gmail.com>
2024-09-09 21:22:16 -0400

    Update the script

--- no_driver.py
+++ no_driver.py
@@ -1,27 +1,16 @@
 import nodriver
 import time
-import os
-from sbvirtualdisplay.display import Display
-import pyautogui
-import Xlib.display
+from sbvirtualdisplay import Display
 
 
 async def main():
-    disp = Display(
-        visible=True, size=(1366, 768), backend="xvfb", use_xauth=True
-    )
-    disp.start()
+    display = Display(visible=True, size=(1366, 768))
+    display.start()
     browser = await nodriver.start()
     page = await browser.get("https://www.virtualmanager.com/en/login")
-    pyautogui._pyautogui_x11._display = (
-        Xlib.display.Display(os.environ['DISPLAY'])
-    )
-    print(await page.evaluate("document.title"))
-    time.sleep(3)
-    pyautogui.click(228, 387)
     time.sleep(3)
     print(await page.evaluate("document.title"))
-    disp.stop()
+    display.stop()
 
 if __name__ == "__main__":
     nodriver.loop().run_until_complete(main())
