dda373d7c9f2a230b3ee48af61df2be64c4cdef5
Michael Mintz <mdmintz@gmail.com>
2024-09-09 21:24:20 -0400

    Update a script

--- no_driver.py
+++ no_driver.py
@@ -4,7 +4,7 @@ from sbvirtualdisplay import Display
 
 
 async def main():
-    display = Display(visible=True, size=(1366, 768))
+    display = Display(visible=False, size=(1366, 768))
     display.start()
     browser = await nodriver.start()
     page = await browser.get("https://www.virtualmanager.com/en/login")
