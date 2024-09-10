4441f5dac7c81db43672e50af1dc25a3b02a8536
Michael Mintz <mdmintz@gmail.com>
2024-09-09 20:52:55 -0400

    Update scripts

--- no_driver.py
+++ no_driver.py
@@ -1,11 +1,20 @@
 import nodriver
 import time
+import os
 from pyvirtualdisplay.display import Display
+import pyautogui
+import Xlib.display
 
 
 async def main():
     browser = await nodriver.start()
     page = await browser.get("https://www.virtualmanager.com/en/login")
+    pyautogui._pyautogui_x11._display = (
+        Xlib.display.Display(os.environ['DISPLAY'])
+    )
+    print(await page.evaluate("document.title"))
+    time.sleep(3)
+    pyautogui.click(228, 387)
     time.sleep(3)
     print(await page.evaluate("document.title"))
 
--- test_user_agent.py
+++ test_user_agent.py
@@ -9,34 +9,3 @@ class OfflineTests(BaseCase):
         self.open("data:,")
         user_agent = self.get_user_agent()
         print('\nUser Agent = "%s"' % user_agent)
-
-        # Now change the user-agent using "execute_cdp_cmd()"
-        if not self.is_chromium():
-            msg = "\n* execute_cdp_cmd() is only for Chromium browsers"
-            print(msg)
-            self.skip(msg)
-        print("\n--------------------------")
-        if not self.headless:
-            self.open("chrome://version/")
-            self.highlight("#useragent", loops=8)
-        try:
-            self.execute_cdp_cmd(
-                "Network.setUserAgentOverride",
-                {
-                    "userAgent": "Mozilla/5.0 "
-                    "(Nintendo Switch; WifiWebAuthApplet) "
-                    "AppleWebKit/606.4 (KHTML, like Gecko) "
-                    "NF/6.0.1.15.4 NintendoBrowser/5.1.0.20393"
-                },
-            )
-            new_user_agent = self.get_user_agent()
-            print('\nOverrided User Agent = "%s"' % new_user_agent)
-        finally:
-            # Reset the user-agent back to the original
-            self.execute_cdp_cmd(
-                "Network.setUserAgentOverride",
-                {"userAgent": user_agent},
-            )
-        print("\n--------------------------")
-        user_agent = self.get_user_agent()
-        print('\nUser Agent = "%s"' % user_agent)
