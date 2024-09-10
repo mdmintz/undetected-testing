54f4a8d97a5f14ff8f8b00026943802d1ce38dd4
Michael Mintz <mdmintz@gmail.com>
2024-09-10 14:25:22 -0400

    Update scripts

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -4,8 +4,8 @@ from seleniumbase import SB
 with SB(uc=True, test=True) as sb:
     import pyautogui
     url = "https://www.virtualmanager.com/en/login"
-    sb.uc_open_with_disconnect(url)
-    sb.sleep(6)
+    sb.uc_open_with_reconnect(url, 6)
+    sb.disconnect()
     pyautogui.moveTo(228, 387, 1.05, pyautogui.easeOutQuad)
     sb.sleep(0.056)
     pyautogui.click()
--- test_user_agent.py
+++ test_user_agent.py
@@ -1,11 +1,10 @@
 import pytest
 from seleniumbase import BaseCase
-BaseCase.main(__name__, __file__, "--uc", "--xvfb")
+BaseCase.main(__name__, __file__, "--uc")
 
 
 @pytest.mark.offline  # Can be run with: "pytest -m offline"
 class OfflineTests(BaseCase):
     def test_get_user_agent(self):
-        self.open("data:,")
         user_agent = self.get_user_agent()
         print('\nUser Agent = "%s"' % user_agent)
