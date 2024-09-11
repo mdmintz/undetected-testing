282ba73fa90c1212dee51d823dc3107592c069ea
Michael Mintz <mdmintz@gmail.com>
2024-09-10 20:35:10 -0400

    Update scripts

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -1,16 +1,8 @@
 from seleniumbase import SB
-from sbvirtualdisplay import Display
-import os
 
-display = Display(
-    visible=True, size=(1366, 768), backend="xvfb", use_xauth=True
-)
-display.start()
-os.environ["DISPLAY"] = f":{display.display}"
 with SB(uc=True, headed=True) as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.uc_open_with_reconnect(url, 10)
     sb.uc_gui_click_captcha()
     sb.sleep(5)
     print(sb.get_page_title())
-display.stop()
