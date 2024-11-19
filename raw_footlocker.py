b0569a07c5a732b67146e3e2ae3cf6270ca7e481
Michael Mintz <mdmintz@gmail.com>
2024-11-19 08:56:01 -0500

    Update examples

---
+++ raw_footlocker.py
@@ -0,0 +1,22 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True, locale_code="en", ad_block=True) as sb:
+    url = "https://www.footlocker.com/"
+    sb.activate_cdp_mode(url)
+    sb.sleep(2.5)
+    sb.cdp.click_if_visible('button[id*="Agree"]')
+    sb.sleep(2.5)
+    sb.cdp.mouse_click('input[aria-label="Search"]')
+    sb.sleep(2.5)
+    search = "Nike Shoes"
+    sb.cdp.press_keys('input[aria-label="Search"]', search)
+    sb.sleep(2.5)
+    sb.cdp.mouse_click('ul[id*="typeahead"] li div')
+    sb.sleep(3.5)
+    elements = sb.cdp.select_all("a.ProductCard-link")
+    if elements:
+        print('**** Found results for "%s": ****' % search)
+    for element in elements:
+        print("------------------ >>>")
+        print("* " + element.text)
+    sb.sleep(2)
