2b2725c57f278a6f2bf30334f213fbcfb51276fb
Michael Mintz <mdmintz@gmail.com>
2025-01-29 13:00:40 -0500

    Update an example

--- raw_chatgpt.py
+++ raw_chatgpt.py
@@ -1,12 +1,15 @@
 from contextlib import suppress
 from seleniumbase import SB
 
-with SB(uc=True, test=True, ad_block=True) as sb:
+with SB(uc=True, test=True) as sb:
     url = "https://chatgpt.com/"
     sb.activate_cdp_mode(url)
-    sb.sleep(1)
     query = "Compare Playwright to SeleniumBase in under 178 words"
-    sb.type("#prompt-textarea", query)
+    try:
+        sb.type("#prompt-textarea", query)
+    except Exception:
+        print(sb.get_page_source())
+        sb.press_keys("#prompt-textarea", query)
     sb.click('button[data-testid="send-button"]')
     print('*** Input for ChatGPT: ***\n"%s"' % query)
     with suppress(Exception):
