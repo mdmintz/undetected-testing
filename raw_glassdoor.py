87187802348b519cc9a58cc27fb1010e988dbdea
Michael Mintz <mdmintz@gmail.com>
2025-01-29 14:36:42 -0500

    Update an example

--- raw_glassdoor.py
+++ raw_glassdoor.py
@@ -1,8 +1,9 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True, ad_block=True) as sb:
+with SB(uc=True, test=True, incognito=True) as sb:
     url = "https://www.glassdoor.com/Reviews/index.htm"
     sb.activate_cdp_mode(url)
+    sb.sleep(1.5)
     sb.uc_gui_click_captcha()
     sb.highlight('[data-test="global-nav-glassdoor-logo"]')
     sb.highlight('[data-test="site-header-companies"]')
