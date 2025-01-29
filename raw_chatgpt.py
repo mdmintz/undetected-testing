7ee0e25114d2a9806731e4ea463bcb115cb01c26
Michael Mintz <mdmintz@gmail.com>
2025-01-29 13:09:46 -0500

    Update the ChatGPT example

--- raw_chatgpt.py
+++ raw_chatgpt.py
@@ -1,7 +1,7 @@
 from contextlib import suppress
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, incognito=True) as sb:
     url = "https://chatgpt.com/"
     sb.activate_cdp_mode(url)
     query = "Compare Playwright to SeleniumBase in under 178 words"
@@ -10,12 +10,15 @@ with SB(uc=True, test=True) as sb:
     except Exception:
         print(sb.get_page_source())
         sb.press_keys("#prompt-textarea", query)
-    sb.click('button[data-testid="send-button"]')
+    sb.cdp.gui_click_element('button[data-testid="send-button"]')
     print('*** Input for ChatGPT: ***\n"%s"' % query)
     with suppress(Exception):
         # The "Send" button reappears when ChatGPT is done typing a response
         sb.wait_for_element('button[data-testid="send-button"]', timeout=22)
-    chat = sb.find_element('[data-message-author-role="assistant"] .markdown')
+    try:
+        chat = sb.find_element('[data-message-author-role="assistant"]')
+    except Exception:
+        chat = sb.find_element("main.relative")
     soup = sb.get_beautiful_soup(chat.get_html()).get_text("\n").strip()
     print("*** Response from ChatGPT: ***\n%s" % soup.replace("\n:", ":"))
     sb.sleep(3)
