e3b8399122d73ad307bd9c01171c7d8f49e66025
Michael Mintz <mdmintz@gmail.com>
2025-02-07 12:17:56 -0500

    Update an example

--- raw_indeed.py
+++ raw_indeed.py
@@ -1,28 +1,36 @@
-from seleniumbase import SB
+import asyncio
+from bs4 import BeautifulSoup as Soup
+from seleniumbase.core import sb_cdp
+from seleniumbase.undetected import cdp_driver
 
-with SB(uc=True, test=True) as sb:
-    url = "https://www.indeed.com/companies"
-    company = "NASA Jet Propulsion Laboratory"
-    search_box = 'input[data-testid="company-search-box"]'
-    sb.activate_cdp_mode(url)
-    sb.sleep(2)
-    if not sb.is_element_present(search_box):
-        sb.uc_gui_click_captcha()
-        sb.sleep(2)
-    sb.press_keys(search_box, company)
-    sb.click('button[type="submit"]')
-    if not sb.is_element_present('a:contains("%s")' % company):
-        sb.uc_gui_click_captcha()
-        sb.sleep(2)
-    sb.click('a:contains("%s")' % company)
-    sb.sleep(3)
-    sb.cdp.highlight('div[itemprop="name"]')
-    sb.sleep(1)
-    sb.cdp.highlight('h2:contains("About the company")')
-    sb.sleep(2)
-    for i in range(10):
-        sb.cdp.scroll_down(12)
-        sb.sleep(0.14)
-    info = sb.find_element('[data-testid="AboutSection-section"]')
-    soup = sb.get_beautiful_soup(info.get_html()).get_text("\n").strip()
-    print("*** %s: ***\n%s" % (company, soup.replace("\n:", ":")))
+url = "https://www.indeed.com/companies"
+loop = asyncio.new_event_loop()
+driver = cdp_driver.cdp_util.start_sync()
+page = loop.run_until_complete(driver.get(url))
+sb = sb_cdp.CDPMethods(loop, page, driver)
+
+company = "NASA Jet Propulsion Laboratory"
+search_box = 'input[data-testid="company-search-box"]'
+captcha_grid = '[style="display: grid;"]'
+
+if sb.is_element_present(captcha_grid):
+    sb.gui_click_element(captcha_grid)
+
+sb.press_keys(search_box, company)
+sb.click('button[type="submit"]')
+if not sb.is_element_present('a:contains("%s")' % company):
+    if sb.is_element_present(captcha_grid):
+        sb.gui_click_element(captcha_grid)
+
+sb.click('a:contains("%s")' % company)
+sb.sleep(3)
+sb.highlight('div[itemprop="name"]')
+sb.sleep(1)
+sb.highlight('h2:contains("About the company")')
+sb.sleep(2)
+for i in range(10):
+    sb.scroll_down(12)
+    sb.sleep(0.14)
+info = sb.find_element('[data-testid="AboutSection-section"]')
+soup = Soup(info.get_html()).get_text("\n").strip()
+print("*** %s: ***\n%s" % (company, soup.replace("\n:", ":")))
