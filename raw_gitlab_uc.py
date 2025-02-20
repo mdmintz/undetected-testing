485b0d872f6186731c50ec52a975f44eccb1f3b0
Michael Mintz <mdmintz@gmail.com>
2025-02-20 08:43:33 -0500

    Update GitHub Actions

---
+++ raw_gitlab_uc.py
@@ -0,0 +1,14 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True) as sb:
+    url = "https://gitlab.com/users/sign_in"
+    sb.uc_open_with_reconnect(url, 5)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
+    sb.uc_gui_handle_captcha()
+    sb.assert_text("Username", '[for="user_login"]', timeout=3)
+    sb.assert_element('label[for="user_login"]')
+    sb.highlight('button:contains("Sign in")')
+    sb.highlight('h1:contains("GitLab.com")')
+    sb.post_message("SeleniumBase wasn't detected", duration=4)
+    print("Success! Website did not detect SeleniumBase!")
