d7a28539bd9bc1f451c58b696449b6e32d7cd4d9
Michael Mintz <mdmintz@gmail.com>
2025-02-20 09:02:00 -0500

    Update an example

--- raw_demo_script.py
+++ raw_demo_script.py
@@ -1,9 +1,9 @@
 """UC Mode has PyAutoGUI methods for CAPTCHA-bypass."""
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(test=True) as sb:
     url = "https://my-user-agent.com/"
-    sb.uc_open(url)
+    sb.open(url)
     sb.highlight("#ua_string")
     user_agent_detected = sb.get_text("#ua_string")
     print("\n\nUser-Agent: %s\n\n" % user_agent_detected)
