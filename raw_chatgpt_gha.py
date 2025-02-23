af249f7edfb3da1da5bd96d01129ee99e2efc83a
Michael Mintz <mdmintz@gmail.com>
2025-02-23 11:18:29 -0500

    Update an example

--- raw_chatgpt_gha.py
+++ raw_chatgpt_gha.py
@@ -1,22 +1,27 @@
-from contextlib import suppress
 from seleniumbase import SB
 
-with SB(uc=True, test=True, ad_block=True) as sb:
+with SB(uc=True, test=True, locale="en-US") as sb:
     url = "https://chatgpt.com/"
     sb.uc_open_with_disconnect(url)
-    sb.sleep(2.5)
+    sb.sleep(3)
     query = "Compare Playwright to SeleniumBase in under 178 words"
-    sb.uc_gui_press_keys(query + "\n")
+    sb.uc_gui_write(query)
+    sb.uc_gui_press_key("ENTER")
     print('*** Input for ChatGPT: ***\n"%s"' % query)
-    sb.sleep(7.5)
+    sb.sleep(10)
     sb.connect()
-    with suppress(Exception):
-        # The "Stop" button disappears when ChatGPT is done typing a response
-        sb.wait_for_element_not_visible(
-            'button[data-testid="stop-button"]', timeout=12
+    success = False
+    try:
+        chat = sb.find_element(
+            '[data-message-author-role="assistant"] .markdown'
         )
-    chat = sb.find_element('[data-message-author-role="assistant"] .markdown')
-    soup = sb.get_beautiful_soup(chat.get_attribute("outerHTML"))
-    soup = soup.get_text("\n").strip()
-    print("*** Response from ChatGPT: ***\n%s" % soup.replace("\n:", ":"))
+        soup = sb.get_beautiful_soup(chat.get_attribute("outerHTML"))
+        soup = soup.get_text("\n").strip()
+        print("*** Response from ChatGPT: ***\n%s" % soup.replace("\n:", ":"))
+        success = True
+    except Exception:
+        print(sb.get_page_source())
+    finally:
+        if not success:
+            raise
     sb.sleep(1)
