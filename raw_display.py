02eb333b6bb8dc03e45138e9b18547fe87722106
Michael Mintz <mdmintz@gmail.com>
2025-05-10 13:42:52 -0400

    Update a script

--- raw_display.py
+++ raw_display.py
@@ -1,4 +1,3 @@
-import pyautogui
 from seleniumbase import SB
 
 with SB(
@@ -7,4 +6,5 @@ with SB(
     url = "https://seleniumbase.io/apps/turnstile"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
+    import pyautogui
     print(pyautogui.size())
