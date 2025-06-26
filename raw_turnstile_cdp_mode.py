d32e7ce574d16fd2873bfeb60993ee8638cd2716
Michael Mintz <mdmintz@gmail.com>
2025-06-26 15:53:03 -0400

    Update an example

--- raw_turnstile_cdp_mode.py
+++ raw_turnstile_cdp_mode.py
@@ -3,8 +3,8 @@ from seleniumbase import SB
 with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/turnstile"
     sb.activate_cdp_mode(url)
-    sb.sleep(2)
-    sb.uc_gui_click_captcha()
+    sb.sleep(1)
+    sb.cdp.gui_click_element("div.cf-turnstile")
     sb.assert_element("img#captcha-success", timeout=3)
     sb.set_messenger_theme(location="top_left")
     sb.post_message("SeleniumBase wasn't detected", duration=3)
