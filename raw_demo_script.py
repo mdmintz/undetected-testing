e10e815c7d0bc6542d4276161bcb611c73b705a3
Michael Mintz <mdmintz@gmail.com>
2025-06-30 08:43:50 -0400

    Update an example

--- raw_demo_script.py
+++ raw_demo_script.py
@@ -7,8 +7,3 @@ with SB(test=True) as sb:
     sb.highlight("#ua_string")
     user_agent_detected = sb.get_text("#ua_string")
     print("\n\nUser-Agent: %s\n\n" % user_agent_detected)
-
-with SB(uc=True, incognito=True, test=True) as sb:
-    sb.driver.uc_open_with_reconnect("https://pixelscan.net/", 10)
-    sb.remove_elements("jdiv")  # Remove chat widgets
-    print(sb.get_text())
