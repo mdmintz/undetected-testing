68a07a40a599729cc9424dcbf82e728d14ee3625
Michael Mintz <mdmintz@gmail.com>
2025-06-29 22:56:44 -0400

    Add an example

---
+++ raw_upwork_cdp_mode.py
@@ -0,0 +1,11 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True) as sb:
+    url = "https://www.upwork.com/nx/search/jobs/"
+    sb.activate_cdp_mode(url)
+    sb.sleep(2.5)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2.5)
+    sb.press_keys('input[type="search"]', "WordPress\n")
+    sb.sleep(2.5)
+    print(sb.get_text('[data-test="JobsList"]'))
