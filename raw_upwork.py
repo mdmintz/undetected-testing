e18d604e9547b4e494edac75f24080416e663427
Michael Mintz <mdmintz@gmail.com>
2025-06-30 08:18:44 -0400

    Update an example

--- raw_upwork.py
+++ raw_upwork.py
@@ -1,12 +1,20 @@
 from seleniumbase import SB
+from seleniumbase import config as sb_config
 
 with SB(uc=True, test=True) as sb:
     url = "https://www.upwork.com/nx/search/jobs/"
-    sb.uc_open_with_reconnect(url, reconnect_time=3)
+    sb.uc_open_with_reconnect(url, 3)
     sb.uc_gui_click_captcha()
-    sb.sleep(2.5)
-    sb.uc_gui_click_captcha()
-    sb.sleep(2.5)
+    sb.sleep(3)
+    if (
+        hasattr(sb_config, "_saved_cf_x_y")
+        and not sb.is_element_visible('input[type="search"]')
+    ):
+        x, y = sb_config._saved_cf_x_y
+        sb.uc_open_with_disconnect(url, 3)
+        sb.uc_gui_click_x_y(x, y)
+        sb.sleep(3)
+        sb.reconnect()
     sb.press_keys('input[type="search"]', "WordPress\n")
-    sb.sleep(2.5)
+    sb.sleep(2)
     print(sb.get_text('[data-test="JobsList"]'))
