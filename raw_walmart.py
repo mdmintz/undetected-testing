9bb9d97af758189337ac2b4b67d4b6e91e042ef6
Michael Mintz <mdmintz@gmail.com>
2025-07-29 22:39:49 -0400

    Update an example

--- raw_walmart.py
+++ raw_walmart.py
@@ -1,15 +1,18 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True, locale_code="en", ad_block=True) as sb:
+with SB(uc=True, test=True, ad_block=True) as sb:
     url = "https://www.walmart.com/"
     sb.activate_cdp_mode(url)
     sb.sleep(2.5)
+    sb.cdp.click_if_visible('[data-automation-id*="close-mark"]')
+    sb.sleep(0.3)
     sb.cdp.mouse_click('input[aria-label="Search"]')
     sb.sleep(1.2)
     search = "Settlers of Catan Board Game"
     required_text = "Catan"
     sb.cdp.press_keys('input[aria-label="Search"]', search + "\n")
     sb.sleep(3.8)
+    sb.cdp.remove_elements('[data-testid="skyline-ad"]')
     print('*** Walmart Search for "%s":' % search)
     print('    (Results must contain "%s".)' % required_text)
     unique_item_text = []
@@ -17,7 +20,7 @@ with SB(uc=True, test=True, locale_code="en", ad_block=True) as sb:
     for item in items:
         if required_text in item.text:
             description = item.querySelector(
-                '[data-automation-id="product-price"] + span'
+                '[data-automation-id="product-title"]'
             )
             if description and description.text not in unique_item_text:
                 unique_item_text.append(description.text)
