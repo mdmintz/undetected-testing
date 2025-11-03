22f0fdf3825e7e7319206a8e66943bd15f9d693b
Michael Mintz <mdmintz@gmail.com>
2025-11-03 11:55:00 -0500

    Update comments

--- raw_ipify_gha.py
+++ raw_ipify_gha.py
@@ -8,21 +8,23 @@ with SB() as sb:
     print(sb.get_page_source())
 
 try:
+    # Get proxy from GitHub Secrets
     proxy = os.environ["proxy_1"]
 except Exception:
     proxy = None
 
 if proxy:
+    # Confirm that proxy was obtained from GitHub Secrets
     print("First part of proxy IP:")
     print(proxy.split("@")[-1].split(":")[0].split(".")[0])
 
-# Regular Mode with proxy
+# Regular Mode with proxy from GitHub Secrets
 with SB(proxy=proxy) as sb:
     sb.open("https://api.ipify.org")
     print("Full IP from ipify:")
     print(sb.get_page_source())
 
-# CDP Mode
+# CDP Mode with proxy from GitHub Secrets
 with SB(proxy=proxy, uc=True) as sb:
     sb.activate_cdp_mode("https://api.ipify.org")
     print("Full IP from ipify:")
