e8ea854cd07f5975bab96607fb83d470d6d55b57
Michael Mintz <mdmintz@gmail.com>
2025-11-03 00:59:37 -0500

    Update GitHub Actions

--- raw_ipify_gha.py
+++ raw_ipify_gha.py
@@ -1,16 +1,29 @@
 import os
-from seleniumbase import Driver
+from seleniumbase import SB
+
+# Regular Mode (no proxy)
+with SB() as sb:
+    sb.open("https://api.ipify.org")
+    print("Full IP from ipify:")
+    print(sb.get_page_source())
 
 try:
     proxy = os.environ["proxy_1"]
 except Exception:
     proxy = None
 
-print("First part of proxy IP:")
-print(proxy.split("@")[-1].split(":")[0].split(".")[0])
+if proxy:
+    print("First part of proxy IP:")
+    print(proxy.split("@")[-1].split(":")[0].split(".")[0])
 
-driver = Driver(proxy=proxy, uc=True)
-driver.uc_activate_cdp_mode("https://api.ipify.org")
-print("Full IP from ipify:")
-print(driver.page_source)
-driver.quit()
+# Regular Mode with proxy
+with SB(proxy=proxy) as sb:
+    sb.open("https://api.ipify.org")
+    print("Full IP from ipify:")
+    print(sb.get_page_source())
+
+# CDP Mode
+with SB(proxy=proxy, uc=True) as sb:
+    sb.activate_cdp_mode("https://api.ipify.org")
+    print("Full IP from ipify:")
+    print(sb.get_page_source())
