9c99d78ac5da93652b08460d87f6edbd637d6bcb
Michael Mintz <mdmintz@gmail.com>
2025-11-03 00:34:26 -0500

    Update examples

--- raw_ipify_gha.py
+++ raw_ipify_gha.py
@@ -9,8 +9,8 @@ except Exception:
 print("First part of proxy IP:")
 print(proxy.split("@")[-1].split(":")[0].split(".")[0])
 
-driver = Driver(proxy=proxy)
-driver.get("https://api.ipify.org")
+driver = Driver(proxy=proxy, uc=True)
+driver.uc_activate_cdp_mode("https://api.ipify.org")
 print("Full IP from ipify:")
 print(driver.page_source)
 driver.quit()
