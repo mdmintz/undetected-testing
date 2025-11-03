697330bb8a7b6d38a058d84294330c11943d4011
Michael Mintz <mdmintz@gmail.com>
2025-11-02 20:09:30 -0500

    Update an example

--- raw_ipify_gha.py
+++ raw_ipify_gha.py
@@ -6,7 +6,11 @@ try:
 except Exception:
     proxy = None
 
+print("First part of proxy IP:")
+print(proxy.split("@")[-1].split(":")[0].split(".")[0])
+
 driver = Driver(proxy=proxy)
 driver.get("https://api.ipify.org")
+print("Full IP from ipify:")
 print(driver.page_source)
 driver.quit()
