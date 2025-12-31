fbf975aa132230ec7045c8914823665a208d4dbd
Michael Mintz <mdmintz@gmail.com>
2025-12-31 15:40:27 -0500

    Update a Stealthy Playwright example

--- playwright/raw_planetmc_sync.py
+++ playwright/raw_planetmc_sync.py
@@ -4,12 +4,15 @@ from seleniumbase import sb_cdp
 sb = sb_cdp.Chrome()
 endpoint_url = sb.get_endpoint_url()
 
-with sync_playwright() as p:
-    browser = p.chromium.connect_over_cdp(endpoint_url)
-    context = browser.contexts[0]
-    page = context.pages[0]
-    page.goto("https://www.planetminecraft.com/account/sign_in/")
-    sb.sleep(2)
-    sb.solve_captcha()
-    sb.wait_for_element_absent("input[disabled]")
-    sb.sleep(2)
+try:
+    with sync_playwright() as p:
+        browser = p.chromium.connect_over_cdp(endpoint_url)
+        context = browser.contexts[0]
+        page = context.pages[0]
+        page.goto("https://www.planetminecraft.com/account/sign_in/")
+        sb.sleep(2)
+        sb.solve_captcha()
+        sb.wait_for_element_absent("input[disabled]")
+        sb.sleep(2)
+except Exception as e:
+    print(e)
