2c52c47785cbd3370115c6a4919b1686391bd95d
Michael Mintz <mdmintz@gmail.com>
2025-12-31 15:28:28 -0500

    Update Stealthy Playwright examples

--- playwright/raw_nike_sync.py
+++ playwright/raw_nike_sync.py
@@ -4,19 +4,22 @@ from seleniumbase import sb_cdp
 sb = sb_cdp.Chrome()
 endpoint_url = sb.get_endpoint_url()
 
-with sync_playwright() as p:
-    browser = p.chromium.connect_over_cdp(endpoint_url)
-    context = browser.contexts[0]
-    page = context.pages[0]
-    page.goto("https://www.nike.com/")
-    page.click('[data-testid="user-tools-container"] search')
-    search = "Pegasus"
-    page.fill('input[type="search"]', search)
-    sb.sleep(4)
-    details = 'ul[data-testid*="products"] figure .details'
-    items = page.locator(details)
-    if items:
-        print('**** Found results for "%s": ****' % search)
-    for i in range(items.count()):
-        item = items.nth(i)
-        print(item.inner_text())
+try:
+    with sync_playwright() as p:
+        browser = p.chromium.connect_over_cdp(endpoint_url)
+        context = browser.contexts[0]
+        page = context.pages[0]
+        page.goto("https://www.nike.com/")
+        page.click('[data-testid="user-tools-container"] search')
+        search = "Pegasus"
+        page.fill('input[type="search"]', search)
+        sb.sleep(4)
+        details = 'ul[data-testid*="products"] figure .details'
+        items = page.locator(details)
+        if items:
+            print('**** Found results for "%s": ****' % search)
+        for i in range(items.count()):
+            item = items.nth(i)
+            print(item.inner_text())
+except Exception as e:
+    print(e)
