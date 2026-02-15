09e16dcfc1a74883b897b9e0af876e7846922492
Michael Mintz <mdmintz@gmail.com>
2026-02-15 08:58:17 -0500

    Update the ChatGPT example

--- raw_chatgpt.py
+++ raw_chatgpt.py
@@ -1,15 +1,12 @@
 from contextlib import suppress
 from seleniumbase import SB
 
-with SB(uc=True, test=True) as sb:
+with SB(uc=True, test=True, ad_block=True) as sb:
     url = "https://chatgpt.com/"
     sb.activate_cdp_mode(url)
     sb.sleep(1)
-    sb.uc_gui_click_captcha()
-    sb.sleep(1)
-    sb.uc_gui_handle_captcha()
-    sb.sleep(1)
     sb.click_if_visible('button[aria-label="Close dialog"]')
+    sb.click_if_visible('button[data-testid="close-button"]')
     query = "Compare Playwright to SeleniumBase in under 178 words"
     sb.press_keys("#prompt-textarea", query)
     sb.click('button[data-testid="send-button"]')
@@ -21,8 +18,7 @@ with SB(uc=True, test=True) as sb:
             'button[data-testid="stop-button"]', timeout=20
         )
     chat = sb.find_element('[data-message-author-role="assistant"] .markdown')
-    soup = sb.get_beautiful_soup(chat.get_html()).get_text("\n").strip()
-    soup = soup.replace("\n\n\n", "\n\n")
+    soup = sb.get_beautiful_soup(chat.get_html()).text.strip()
     soup = soup.replace("\n\n\n", "\n\n")
     print("*** Response from ChatGPT: ***\n%s" % soup)
     sb.sleep(3)
