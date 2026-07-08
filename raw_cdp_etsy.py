f1cc0c06ffc914b0b7a09b85d7f0dff6347492a4
Michael Mintz <mdmintz@gmail.com>
2026-07-08 10:33:38 -0400

    Update an example

--- raw_cdp_etsy.py
+++ raw_cdp_etsy.py
@@ -3,13 +3,17 @@ from seleniumbase import sb_cdp
 try:
     sb = sb_cdp.Chrome(ad_block=True)
     sb.goto("https://www.etsy.com/")
-    sb.sleep(1)
+    sb.sleep(2)
     search = "FIFA Keychains"
     required_text = "keychain"
+    sb.solve_captcha()
+    sb.sleep(1)
     sb.type('input[data-id="search-query"]', search)
     sb.sleep(1)
     sb.click('button[aria-label="Search"]')
     sb.sleep(2)
+    sb.solve_captcha()
+    sb.sleep(1)
     sb.click_if_visible('button[aria-label="Ok"]')
     soup = sb.get_beautiful_soup()
     items = soup.select("div.v2-listing-card__info")
