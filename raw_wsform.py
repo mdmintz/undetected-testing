05226ea8beea215df78d600c79ba67aa98afe2f9
Michael Mintz <mdmintz@gmail.com>
2026-07-15 18:18:36 -0400

    Update GitHub Actions

---
+++ raw_wsform.py
@@ -0,0 +1,9 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True, locale="en", incognito=True) as sb:
+    sb.activate_cdp_mode()
+    sb.goto("https://wsform.com/demo/")
+    sb.sleep(2.5)
+    sb.solve_captcha()  # PyAutoGUI mouse click
+    sb.sleep(2.5)
+    sb.save_screenshot_to_logs()
